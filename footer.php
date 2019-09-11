abc8d20176428adc1f32f00ce5d2f9173f8d64cb
Salem DJEGHBALA <sdjeghba@e1r13p13.42.fr>
2019-09-11 06:01:56 +0200

    first

---
+++ footer.php
@@ -0,0 +1,20 @@
+<<<HTML
+        <form action="dashboard.php" class="form-inline">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                </div>
+            </div>
+            <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+            <input type="password" class="form-control" placeholder="Password">
+            <button class="btn btn-primary">Se connecter</button>
+        </form>
+HTML;
+
+
+<nav class="navbar navbar-dark bg-dark">
+    <a href="#" class="navbar-brand">
+        <img src="src/img/camera.png" width="30" height="30" alt="icon camera" class="d-inline-block align-top">
+    Camagru
+    </a>
+    </nav>
