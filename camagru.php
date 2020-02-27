b1429efc933ba725b0ad618bcfdc76d0b870f73f
saremu <saremu@MacBook-Pro-de-saremu.local>
2020-02-27 20:32:05 +0100

    find the way to take picture and save them

--- camagru.php
+++ camagru.php
@@ -3,31 +3,34 @@
 <body>
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-8 mx-4 my-4 text-center">
+            <div class="col-7 mx-4 my-4 text-center">
                 <div class="card border-primary">
                     <div class="card-header bg-primary text-white">
                      Prenez vous en photo !
                     </div>
-                    <div class="card-body" id="webcam_container">
-                        <video autoplay="true" id="videoElement">
-                            <script src="/webcam.js"></script>
-                        </video>
-                        <button id="startbutton">Take photo</button>
-                    </div>  
-                </div>
+                    <div class="card-body my justify-content-center">
+                        <div class="webcam_container">
+                            <video autoplay="true" id="videoElement"></video>
+                        </div>
+                        <br>
+                        <button type="button" id="capture" class="btn btn-primary">Capture !</button>
+                    </div>
+
+                </div>       
             </div>
-            <div class="col-3 mx-3 my-4 text-center">
+            <div class="col-4 mx-3 my-4 text-center">
                 <div class="card border-primary">
                     <div class="card-header bg-primary text-white">
                      Vos dernières prises photos
                     </div>
                     <div class="card-body">
-                        <p class="success">Done!</p>
+                        <canvas id="picture" class="canvas"></canvas>   
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+                            <script src="/script.js"></script>
 
 <?php require_once __DIR__ . "/content/layout/footer.php"?>
--- webcam.js
+++ webcam.js
@@ -10,32 +10,33 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
-video.addEventListener('canplay', function(ev){
-  if (!streaming) {
-    height = video.videoHeight / (video.videoWidth/width);
-    video.setAttribute('width', width);
-    video.setAttribute('height', height);
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);
-    streaming = true;
-  }
-}, false);
 
-function takepicture() {
-  canvas.width = width;
-  canvas.height = height;
-  canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-  var data = canvas.toDataURL('image/png');
-  photo.setAttribute('src', data);
-}
+// video.addEventListener('canplay', function(ev){
+//   if (!streaming) {
+//     height = video.videoHeight / (video.videoWidth/width);
+//     video.setAttribute('width', width);
+//     video.setAttribute('height', height);
+//     canvas.setAttribute('width', width);
+//     canvas.setAttribute('height', height);
+//     streaming = true;
+//   }
+// }, false);
 
-startbutton.addEventListener('click', function(ev){
-    takepicture();
-  ev.preventDefault();
-}, false);
+// function takepicture() {
+//   canvas.width = width;
+//   canvas.height = height;
+//   canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+//   var data = canvas.toDataURL('image/png');
+//   photo.setAttribute('src', data);
+// }
+
+// startbutton.addEventListener('click', function(ev){
+//     takepicture();
+//   ev.preventDefault();
+// }, false);
 
 
-startbutton.addEventListener('click', function(ev){
-  takepicture();
-  ev.preventDefault();
-}, false);
+// startbutton.addEventListener('click', function(ev){
+//   takepicture();
+//   ev.preventDefault();
+// }, false);
